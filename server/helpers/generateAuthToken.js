51789c58aebb53a6b835236cd9641a1623912567
Satyendra gupta <satyendra05cs@gmail.com>
2019-12-30 09:58:51 +0530

    Authentication

--- server/helpers/generateAuthToken.js
+++ server/helpers/generateAuthToken.js
@@ -9,3 +9,14 @@ token = {
 	}
 }
 module.exports = token;
+
+// var jwt = require("jsonwebtoken"),
+// 	config = require("./config")(),
+// 	_ = require("underscore"),
+// 	authToken = {
+// 		"loginUser": function(data) {
+// 			var token = jwt.sign(data, config.secretKey);
+// 			return token;
+// 		}
+// 	}
+// module.exports = authToken;
